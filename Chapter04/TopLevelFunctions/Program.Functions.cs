ce6fcaaff9a9c8f282d652bd61db88a62581d358
Emanuel Alejandro Lujan <ealujan@dc.uba.ar>
2024-08-10 23:18:10 -0300

    subo el principio del capitulo 4

---
+++ Chapter04/TopLevelFunctions/Program.Functions.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+
+partial class Program
+{
+  static void WhatsMyNamespace()
+  {
+    WriteLine("Namespace of Program class: {0}",
+      arg0: typeof(Program).Namespace ?? "null"
+    );
+  }
+}
---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,1 @@
+TimesTable(7, 20);
