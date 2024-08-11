65d0c7d07203a47e58fa37e21cbaafef72675f46
Emanuel A. Luján <elujan.dev@gmail.com>
2024-08-11 03:50:40 -0300

    add escribir funciones secion

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,14 @@
-TimesTable(7, 20);
+// TimesTable(7, 20);
+
+ConfigureConsole();
+
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
+
+// RunCardinalToOrdinal();
+
+// RunFactorial();
+
+// RunFibImperative();
+
+RunFibFunctional();
