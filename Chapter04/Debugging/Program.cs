045bcf58cb08517fb687eb0d0677fc8b44b91a4b
Emanuel A. Luján <elujan.dev@gmail.com>
2024-08-11 04:39:46 -0300

    debugging section

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,16 @@
+﻿Write("Enter a number: ");
+string number = ReadLine()!;
+
+double a = double.Parse(number);
+double b = 2.5;
+double answer = Add(a, b);
+
+// usar hit count en el breakpoint es util para loops al parecer
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine();
+
+double Add(double a, double b)
+{
+  return a + b;
+}
