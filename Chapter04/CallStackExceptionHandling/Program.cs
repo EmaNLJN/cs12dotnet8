2927201e882bff64c67a9b9fb958c61418e6ac49
Emanuel A. Luján <elujan.dev@gmail.com>
2024-08-12 00:39:46 -0300

    se finalizo el capitulo 4

---
+++ Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using CallStackExceptionHandlingLib;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha()
+{
+  WriteLine("In Alpha");
+  Beta();
+}
+
+void Beta()
+{
+  WriteLine("In Beta");
+  try
+  {
+    Processor.Gamma();
+  }
+  catch (Exception ex)
+  {
+    WriteLine($"Caught this: {ex.Message}");
+    // rethrow la excepcion de catch y retiene la informacion original del call stack.
+    throw;
+  }
+}
