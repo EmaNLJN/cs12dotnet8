76c1457875c2b24cfff9e3c4889e3e51f5840ea6
Emanuel Alejandro Lujan <ealujan@dc.uba.ar>
2024-08-15 03:00:39 -0300

    se agrego el capitulo 6

---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+  Name = "Harry",
+  Born = new(year: 2001, month: 3, day: 25,
+    hour: 0, minute: 0, second: 0,
+    offset: TimeSpan.Zero)
+};
+
+harry.WriteToConsole();
+
+Person lamech = new() { Name = "Lamech" };
+Person adah = new() { Name = "Adah" };
+Person zillah = new() { Name = "Zillah" };
+
+lamech.Marry(adah);
+
+// Person.Marry(lamech, zillah);
+if (lamech + zillah)
+{
+  WriteLine($"{lamech.Name} and {zillah.Name} successfully got married.");
+}
+
+lamech.OutputSpouses();
+adah.OutputSpouses();
+
+Person baby1 = lamech.ProcreateWith(adah);
+baby1.Name = "Jabal";
+WriteLine($"{baby1.Name} was born on {baby1.Born}");
+
+Person baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tubalcain";
+
+Person baby3 = lamech * adah;
+baby3.Name = "Jubal";
+
+Person baby4 = zillah * lamech;
+baby4.Name = "Naamah";
+
+adah.WriteChildrenToConsole();
+zillah.WriteChildrenToConsole();
+lamech.WriteChildrenToConsole();
+
+for(int i = 0; i < lamech.Children.Count; i++)
+{
+  WriteLine(format: "  {0}'s child #{1} is named \"{2}\".",
+  arg0: lamech.Name, arg1: i,
+  arg2: lamech.Children[i].Name);
+}
+
+
