28a1f9c44d45b04448ddb5e97b5a83cb1b26b1ca
Emanuel A. Luján <elujan.dev@gmail.com>
2024-08-16 04:48:31 -0300

    se termino la seccion de null, un poco incompleta

---
+++ Chapter06/NullHandling/Program.cs
@@ -0,0 +1,46 @@
+﻿using Packt.Shared;
+
+int? thisCannotBeNull = 4;
+thisCannotBeNull = null;
+WriteLine(thisCannotBeNull);
+
+WriteLine(thisCannotBeNull);
+WriteLine(thisCannotBeNull.GetValueOrDefault());
+
+thisCannotBeNull = 7;
+
+WriteLine(thisCannotBeNull);
+WriteLine(thisCannotBeNull.GetValueOrDefault());
+
+// esto es lo mismo que int? o sea va a ser de tipo Nullable<int>
+// nullable es un struct por ende es un value type, y tiene sentido con int
+// que tambien es un value type
+Nullable<int> thisCouldAlsoBeNull = null;
+thisCouldAlsoBeNull = 9;
+WriteLine(thisCouldAlsoBeNull);
+
+Address address = new(city: "Londong")
+{
+  Building = null,
+  Street = null!,
+  Region = "UK"
+};
+
+WriteLine(address.Building?.Length);
+
+if (address.Street is not null)
+{
+  WriteLine(address.Street.Length);
+}
+
+string authorName = null;
+int? authorNameLength;
+
+// esto tira una excepcion nullreference
+authorNameLength = authorName.Length;
+
+// en lugar de tirar la excepcion arriba, le asigna un null
+authorNameLength = authorName?.Length;
+
+authorNameLength = authorName?.Length ?? 25;
+
---
+++ Chapter06/PacktLibrary/IPlayable.cs
@@ -0,0 +1,11 @@
+namespace Packt.Shared;
+
+public interface IPlayable
+{
+  void Play();
+  void Pause();
+  void Stop()
+  {
+    WriteLine("Default implementation of Stop.");
+  }
+}
