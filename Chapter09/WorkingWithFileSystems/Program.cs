a228a3202b5317b61fa640d6ad518738f6ec50c6
Emanuel A. Luján <elujan.dev@gmail.com>
2024-08-19 00:03:09 -0300

    se subio algunas cosas de la primer seccion del capitulo 9

--- Chapter09/WorkingWithFileSystems/Program.cs
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -82,4 +82,60 @@ WriteLine("Deleting it...");
 Delete(newFolder, recursive: true);
 WriteLine($"Does it exist? {Path.Exists(newFolder)}");
 
+SectionTitle("Managing files");
+
+string dir = Combine(
+  GetFolderPath(SpecialFolder.Personal), "OutputFiles");
+
+CreateDirectory(dir);
+
+// Define file paths.
+string textFile = Combine(dir, "Dummy.txt");
+string backupFile = Combine(dir, "Dummy.bak");
+WriteLine($"Working with: {textFile}");
+WriteLine($"Does it exists? {File.Exists(textFile)}");
+
+StreamWriter textWriter = File.CreateText(textFile);
+textWriter.WriteLine("Hello, C#!");
+textWriter.Close();
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+File.Copy(sourceFileName: textFile,
+  destFileName: backupFile, overwrite: true);
+
+WriteLine(
+  $"Does {backupFile} exist? {File.Exists(backupFile)}"
+);
+
+Write("Confirm the files exist, and then press any key.");
+ReadKey(intercept: true);
+
+File.Delete(textFile);
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+WriteLine($"Reading contents of {backupFile}");
+StreamReader textReader = File.OpenText(backupFile);
+WriteLine(textReader.ReadToEnd());
+textReader.Close();
+
+SectionTitle("Managing paths");
+
+WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+WriteLine($"File Name: {GetFileName(textFile)}");
+WriteLine("File Name without Extension: {0}",
+  GetFileNameWithoutExtension(textFile));
+WriteLine($"File Extension: {GetExtension(textFile)}");
+WriteLine($"Random File Name: {GetRandomFileName()}");
+WriteLine($"Temporary File Name: {GetTempFileName()}");
+
+SectionTitle("Getting file information");
+
+FileInfo info = new(backupFile);
+WriteLine($"{backupFile}");
+WriteLine($"   Contains {info.Length} bytes.");
+WriteLine($"   Last accessed: {info.LastAccessTime}.");
+WriteLine($"   Has readonly set to {info.IsReadOnly}.");
+
 #endregion
+
+// Recien comienzo "Reading and writing with streams"
